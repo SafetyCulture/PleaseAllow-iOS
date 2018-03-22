a5e470c4c5822e42d421f9691f616099e37108bb
Gagan <gagandeep.singh@safetyculture.com>
2018-03-22 15:54:41 +1100

    Added denied alert to sample

--- Sample/PleaseAllowSample/PleaseAllowSample/ViewController.swift
+++ Sample/PleaseAllowSample/PleaseAllowSample/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     
     @IBAction func allowCamera(_ sender: Any) {
         PleaseAllow.Managers.camera.softAskView = PermissionType.camera.softAskView
+        PleaseAllow.Managers.camera.deniedAlert = PermissionType.camera.deniedView
         PleaseAllow.camera { result, error in
             print(result)
         }
@@ -22,30 +23,35 @@ class ViewController: UIViewController {
     
     @IBAction func allowPhotoLibrary(_ sender: Any) {
         PleaseAllow.Managers.photoLibrary.softAskView = PermissionType.photoLibrary.softAskView
+        PleaseAllow.Managers.photoLibrary.deniedAlert = PermissionType.photoLibrary.deniedView
         PleaseAllow.photoLibrary { result, error in
             print(result)
         }
     }
     @IBAction func allowContacts(_ sender: Any) {
         PleaseAllow.Managers.contacts.softAskView = PermissionType.contacts.softAskView
+        PleaseAllow.Managers.contacts.deniedAlert = PermissionType.contacts.deniedView
         PleaseAllow.contacts { result, error in
             print(result)
         }
     }
     @IBAction func allowLocationWhenInUse(_ sender: Any) {
         PleaseAllow.Managers.location.whenInUse.softAskView = PermissionType.locationWhenInUse.softAskView
+        PleaseAllow.Managers.location.whenInUse.deniedAlert = PermissionType.locationWhenInUse.deniedView
         PleaseAllow.location.whenInUse { result, error in
             print(result)
         }
     }
     @IBAction func allowLocationAlways(_ sender: Any) {
         PleaseAllow.Managers.location.always.softAskView = PermissionType.locationAlways.softAskView
+        PleaseAllow.Managers.location.always.deniedAlert = PermissionType.locationAlways.deniedView
         PleaseAllow.location.always { result, error in
             print(result)
         }
     }
     @IBAction func allowPushNotification(_ sender: Any) {
         PleaseAllow.Managers.push.softAskView = PermissionType.push.softAskView
+        PleaseAllow.Managers.push.deniedAlert = PermissionType.push.deniedView
         PleaseAllow.push { result, error in
             guard !UIApplication.shared.isRegisteredForRemoteNotifications else { return }
             let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
@@ -68,6 +74,14 @@ enum PermissionType {
     
     var denyTitle: String {
         return "Don't Allow"
+    }
+    
+    var settings: String {
+        return "Settings"
+    }
+    
+    var cancel: String {
+        return "Cancel"
     }
     
     var title: String {
@@ -104,6 +118,40 @@ enum PermissionType {
         }
     }
     
+    var deniedTitle: String {
+        switch self {
+        case .camera:
+            return "Camera Denied"
+        case .photoLibrary:
+            return "Photo Library Denied"
+        case .locationWhenInUse:
+            return "Location Denied"
+        case .locationAlways:
+            return "Location Denied"
+        case .contacts:
+            return "Contacts Denied"
+        case .push:
+            return "Push Notifications Denied"
+        }
+    }
+    
+    var deniedDescription: String {
+        switch self {
+        case .camera:
+            return "Open settings and allow access to your Camera in order to add images to your work."
+        case .photoLibrary:
+            return "Open settings and allow access to your Photo Library in order to add images to your work."
+        case .locationWhenInUse:
+            return "Open settings and allow access to your Lcoation in order to add current location to your work."
+        case .locationAlways:
+            return "Open settings and allow access to your Lcoation in order to add current location to your work."
+        case .contacts:
+            return "Open settings and allow access to your Contacts in order to collaborate with others on your work."
+        case .push:
+            return "Open settings and allow us to send you Push Notifications to keep you updated."
+        }
+    }
+    
     var softAskView: SoftAskView {
         let view = SoftAskView()
         view.cornerRadius = 20
@@ -111,6 +159,16 @@ enum PermissionType {
         view.denyButtonTitle = denyTitle
         view.title = title
         view.description = description
+        return view
+    }
+    
+    var deniedView: DeniedAlert {
+        let view = DeniedAlert()
+        view.cornerRadius = 20
+        view.allowButtonTitle = settings
+        view.denyButtonTitle = cancel
+        view.title = deniedTitle
+        view.description = deniedDescription
         return view
     }
 }
